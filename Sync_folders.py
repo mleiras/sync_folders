f05f6c560f6f4bebe4b6fb0bd69c6e5b02dcaafb
mleiras <monicarafaelaleiras@gmail.com>
2024-03-03 23:45:49 +0000

    adding docstrings WIP

--- Sync_folders.py
+++ Sync_folders.py
@@ -5,74 +5,143 @@ import logging
 import hashlib
 import argparse
 
-# command line arguments
 
 def parse_arguments():
+    """
+    Command line arguments parser.
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
     parser = argparse.ArgumentParser(description="Folder Synchronization")
     parser.add_argument("source", help="Path to the source folder")
     parser.add_argument("replica", help="Path to the replica folder")
-    parser.add_argument("--interval", type=int, default=1800, help="Synchronization interval in seconds (default: 1800)")
-    parser.add_argument("--log-file", default="sync_log.txt", help="Path to the log file (default: sync_log.txt)")
+    parser.add_argument(
+        "--interval",
+        type=int,
+        default=1800,
+        help="Synchronization interval in seconds (default: 1800)",
+    )
+    parser.add_argument(
+        "--log-file",
+        default="sync_log.txt",
+        help="Path to the log file (default: sync_log.txt)",
+    )
     return parser.parse_args()
 
 
-# compare files with hash 
+def compare_files(source: str, replica: str) -> bool:
+    """
+    Comparison between two files using sha3 hashing algorithm (hashlib library).
 
-def compare_files(source, replica): # compare hash of files (sha3 more secure)
-    source_hash = hashlib.sha3_256(open(source, 'rb').read()).hexdigest()
-    replica_hash = hashlib.sha3_256(open(replica, 'rb').read()).hexdigest()
+    Parameters
+    ----------
+    source : str
+        Name of file in source folder.
+    replica : str
+        Name of file in replica folder.
+
+    Returns
+    -------
+    bool
+        True if content of the files is identical, False otherwise.
+    """
+    with open(source, "rb") as source_file:
+        source_hash = hashlib.sha3_256(source_file.read()).hexdigest()
+    with open(replica, "rb") as replica_file:
+        replica_hash = hashlib.sha3_256(replica_file.read()).hexdigest()
 
     return source_hash == replica_hash
 
-# compare folders (loop for each file inside each folder)
 
-def compare_folders(source, replica):
-    print(os.listdir(source))
-    print(os.listdir(replica))
+def compare_folders(source: str, replica: str):
+    """
+    Basic logic of comparing two folders and synchronizing files one-way (replica folder needs to be the same as source folder).
+    If needed, it occurs some operations (e.g. copying files from source to replica), and it is logged in the log file.
+
+    Parameters
+    ----------
+    source : str
+        Path of source folder.
+    replica : str
+        Path of replica folder.
+    """
+    # print(os.listdir(source))
+    # print(os.listdir(replica))
 
     for file_replica in os.listdir(replica):
         if file_replica not in os.listdir(source):
             # remove file_replica
-            print(f'2 - File {file_replica} not in source - removing it now.') # file not in source,  remove from replica
-            os.remove(replica+'/'+file_replica)
-            logging.info(f'File {file_replica} removed from replica.')
-    
-    for file in os.listdir(source):
-        if file not in os.listdir(replica): 
-            # copy file to replica dir
-            print(f'1 - File {file} not in replica - copying now. - ADD TO LOG') # file not in replica, copy from source to replica
-            shutil.copy2(source+"/"+file, replica) # copy2 preserves metadata
-            logging.info(f'File {file} copied from source to replica.')
+            print(
+                "2 - File %s not in source - removing it now.", file_replica
+            )  # file not in source,  remove from replica
+            os.remove(replica + "/" + file_replica)
+            logging.info("File %s removed from replica.", file_replica)
 
-        else: # if files with same name, compare hash 
+    for file in os.listdir(source):
+        if file not in os.listdir(replica):
+            # copy file to replica dir
+            print(
+                f"1 - File {file} not in replica - copying now. - ADD TO LOG"
+            )  # file not in replica, copy from source to replica
+            shutil.copy2(source + "/" + file, replica)  # copy2 preserves metadata
+            logging.info("File %s copied from source to replica.", file)
+
+        else:  # if files with same name, compare hash
             for file_replica in os.listdir(replica):
                 if file_replica == file:
-                    if compare_files(source+'/'+file, replica+'/'+file_replica):
-                        print('3 - True - same file. Operations not needed.')
+                    if compare_files(source + "/" + file, replica + "/" + file_replica):
+                        print("3 - True - same file. Operations not needed.")
                     else:
-                        print(f'4 - False - different contents between {file} and {file_replica}. Removing file from replica and copying now the new one. - ADD TO LOG')
+                        print(
+                            "4 - False - different contents between %s and %s. Removing file from replica and copying now the new one. - ADD TO LOG",
+                            file,
+                            file_replica,
+                        )
                         # remove file_replica
-                        os.remove(replica+'/'+file_replica)
-                        logging.info(f'File {file_replica} removed from replica.')
+                        os.remove(replica + "/" + file_replica)
+                        logging.info("File %s removed from replica.", file_replica)
                         # copy file to replica dir
-                        shutil.copy2(source+"/"+file, replica)
-                        logging.info(f'File {file} copied from source to replica.')
+                        shutil.copy2(source + "/" + file, replica)
+                        logging.info("File %s copied from source to replica.", file)
 
     # if dir inside source instead of file, copy also dir with shutil.copytree
     # para remover dir usar shutil.rmtree
 
 
-# main loop with paths and interval as arguments 
+# main loop with paths and interval as arguments
 
-def synchronize_folders(source, replica, interval, log_file):
-    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', filename=log_file, encoding='utf-8', level=logging.DEBUG) # using logging library as is more scalable and standard
-    logging.info(f'STARTING SYNCHRONIZATION with an interval of {interval} seconds.')
+
+def synchronize_folders(source: str, replica: str, interval: int, log_file: str):
+    """
+    Main loop of synchronization between source and replica folders, with a custom period of time and a log file defined.
+
+    Parameters
+    ----------
+    source : str
+        Path of source folder.
+    replica : str
+        Path of replica folder.
+    interval : int
+        Interval in seconds for each synchronization (period of time).
+    log_file : str
+        Path to the log file.
+    """
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(message)s",
+        filename=log_file,
+        encoding="utf-8",
+        level=logging.DEBUG,
+    )  # using logging library as is more scalable and standard
+    logging.info("STARTING SYNCHRONIZATION with an interval of %s seconds.", interval)
     n = 0
-    
+
     while True:
         n += 1
         compare_folders(source, replica)
-        logging.info(f'Synchronization {n} successful.')
+        logging.info("Synchronization %s successful.", n)
         # periodically synchronize folders
         time.sleep(interval)
 
